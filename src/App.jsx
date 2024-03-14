d1698f340c02de5565c173e2637e5abee5206376
iainrm290 <murdochiain17@gmail.com>
2024-03-14 07:51:09 +0000

    Completed 6: View a list of comments associated with an article

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Header from './components/Header'
 import Articles from './components/Articles'
 import ArticlePage from './components/ArticlePage'
+import CommentsPage from './components/CommentsOnArticle'
 import './App.css'
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/articles' element={<Articles />} />
             <Route path='/articles/:article_id' element={<ArticlePage />}/>
-            
+            <Route path='/articles/:article_id/comments' element={<CommentsPage />}/>
         </Routes>
             
         </>
--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,20 +1,25 @@
-import { useParams} from 'react-router-dom'
+import { useParams, Link} from 'react-router-dom'
 import { useEffect, useState } from "react"
 import { getArticleById } from './utils/api'
 
+
 const ArticlePage = () => {
-    const [specificArticle, setSpecificArticle] = useState({})
     const {article_id} = useParams()
-        useEffect(() => {
+    const [specificArticle, setSpecificArticle] = useState([])
+            useEffect(() => {
             getArticleById(article_id).then((article) => {
                 setSpecificArticle(article)
             })
         }, [])
+        
     return (
         <>
             <div id='specific-article-container'>
                 <h2>{specificArticle.title}</h2>
-                <h3>By {specificArticle.author}</h3>
+                <h4>{specificArticle.author}</h4>
+                <h4>{new Date(specificArticle.created_at).toString().substring(3, 15)}</h4>
+                <h4>Votes:{specificArticle.votes}</h4>
+                <h4><Link to={`/articles/${specificArticle.article_id}/comments`}>Comments: </Link>{specificArticle.comment_count}</h4>
                 <img src={specificArticle.article_img_url} />
                 <p>{specificArticle.body}</p>
             </div>
